846d40054582469ab0668e6132023e2bd627caf6
s-tyda <styda@huhha.co>
2025-03-11 13:58:26 +0100

    init repo

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {
+        "msg": "Hello World!"
+    }
+
+
+test_app = TestClient(app)
+
+
+def test_root():
+    response = test_app.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "msg": "Hello World!"
+    }
